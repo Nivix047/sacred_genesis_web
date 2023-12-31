b11edbb1e48fe9067a29f59a9841fc5fcda1427e
Daniel Xu <xudh47@gmail.com>
2023-12-30 18:17:35 -0800

    Cosolidated styling to MUI instead of CSS for media responsiveness

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,87 +1,78 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
+import { Grid, Button, Box, Typography, Container } from "@mui/material";
 import "./Home.css";
 
 const Home = () => {
   return (
-    <div id="home-background-parent">
-      <div className="home-background"></div>
-      <div className="content-section">
-        <Grid container alignItems="center" spacing={2}>
-          <Grid item xs={12} md={6} fontSize={25}>
-            <h1 id="welcome-message-title">Relax, browse and collect</h1>
-            <p id="welcome-message">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
-            </p>
+    <Box id="home-background-parent" sx={{ backgroundColor: "black" }}>
+      <Box className="home-background"></Box>
+      <Box sx={{ paddingTop: 4, paddingBottom: 4 }}>
+        <Container maxWidth="lg">
+          <Grid container alignItems="center" spacing={2}>
+            <Grid item xs={12} md={6}>
+              <Box sx={{ marginY: 2 }}>
+                {" "}
+                <Typography
+                  variant="h4"
+                  gutterBottom
+                  sx={{ mt: 2, px: { xs: 2, sm: 8 }, color: "white" }}
+                >
+                  Relax, browse and collect
+                </Typography>
+                <Typography
+                  variant="body1"
+                  sx={{ px: { xs: 2, sm: 8 }, color: "white" }}
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua...
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={6}
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              <Box
+                className="button-group"
+                sx={{
+                  display: "flex",
+                  gap: 4,
+                  mt: -2,
+                  justifyContent: "center",
+                  flexWrap: "wrap",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{ width: 200, bgcolor: "#5f5f5f" }}
+                >
+                  PURCHASE NFTS
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{ width: 200, bgcolor: "#5f5f5f" }}
+                >
+                  GALLERY
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{ width: 200, bgcolor: "#5f5f5f" }}
+                >
+                  CONTACT ME
+                </Button>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6} md={6} id="right-content-section">
-            <div className="button-group">
-              <Button
-                variant="contained"
-                color="primary"
-                className="styled-button"
-                id="purchase-relics-btn"
-                style={{ padding: "15px 30px" }}
-                sx={{
-                  padding: "15px 30px",
-                  borderColor: "rgba(173, 216, 230, 0.4)",
-                  borderWidth: "2px",
-                  borderStyle: "solid",
-                  ":hover": {
-                    borderColor: "white",
-                  },
-                }}
-              >
-                PURCHASE NFTS
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                className="styled-button"
-                id="gallery-btn"
-                style={{ padding: "15px 30px" }}
-                sx={{
-                  padding: "15px 30px",
-                  borderColor: "rgba(173, 216, 230, 0.4)",
-                  borderWidth: "2px",
-                  borderStyle: "solid",
-                  ":hover": {
-                    borderColor: "white",
-                  },
-                }}
-              >
-                GALLERY
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                className="styled-button"
-                id="analytics-btn"
-                style={{ padding: "30px 30px" }}
-                sx={{
-                  padding: "15px 30px",
-                  borderColor: "rgba(173, 216, 230, 0.4)",
-                  borderWidth: "2px",
-                  borderStyle: "solid",
-                  ":hover": {
-                    borderColor: "white",
-                  },
-                }}
-              >
-                CONTACT ME
-              </Button>
-            </div>
-          </Grid>
-        </Grid>
-      </div>
-    </div>
+        </Container>
+      </Box>
+    </Box>
   );
 };
 
