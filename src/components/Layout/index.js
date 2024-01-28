057f4079b9ac5dc78976f51bcf830ac492e535ea
Daniel Xu <xudh47@gmail.com>
2024-01-27 17:13:18 -0800

    Updated font

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Home from "../pages/Home";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
@@ -6,17 +7,25 @@ import Contact from "../pages/Contact";
 import Terms from "../pages/Terms";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Inconsolata", monospace',
+  },
+});
+
 export default function Layout() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/terms" element={<Terms />} />
-        {/* Add more routes as needed */}
-      </Routes>
-      <Footer />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/terms" element={<Terms />} />
+          {/* Add more routes as needed */}
+        </Routes>
+        <Footer />
+      </Router>
+    </ThemeProvider>
   );
 }
