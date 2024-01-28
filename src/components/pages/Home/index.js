79dd098417c011c09b6f9a2a9a7638a04ed9d573
Daniel Xu <xudh47@gmail.com>
2024-01-28 12:33:51 -0800

    Updated fonts on home page

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -33,7 +33,11 @@ const Home = () => {
                 </Typography>
                 <Typography
                   variant="body1"
-                  sx={{ px: { xs: 2, sm: 2 }, color: "white" }}
+                  sx={{
+                    px: { xs: 2, sm: 2 },
+                    color: "white",
+                    fontSize: "18px",
+                  }}
                 >
                   Dear fellow travelers on the pixelated pilgrimage, welcome to
                   Sacred Genesis. Here, we offer not just NFTs, but existential
@@ -75,7 +79,7 @@ const Home = () => {
                     className="styled-button"
                     id="purchase-nft-btn"
                     sx={{
-                      fontSize: "13px",
+                      fontSize: "15px",
                       textTransform: "none",
                       background: "linear-gradient(180deg, #4a4a4a, #000000)",
                       padding: "15px 30px",
@@ -105,7 +109,7 @@ const Home = () => {
                     className="styled-button"
                     id="purchase-nft-btn"
                     sx={{
-                      fontSize: "13px",
+                      fontSize: "14px",
                       textTransform: "none",
                       background: "linear-gradient(180deg, #4a4a4a, #000000)",
                       padding: "15px 30px",
@@ -128,7 +132,7 @@ const Home = () => {
                   color="primary"
                   onClick={handleContactClick}
                   sx={{
-                    fontSize: "13px",
+                    fontSize: "20px",
                     textTransform: "none",
                     background: "linear-gradient(180deg, #4a4a4a, #000000)",
                     width: 200,
